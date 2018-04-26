771cfac97f757f0ca6273cecc2b5942d406a36be
MateoVM <37589526+MateoVM@users.noreply.github.com>
2018-04-26 22:56:50 +0200

    Addition of krigging_interpolation with a start on the variagram

---
+++ project/krigin_interpolation.m
@@ -0,0 +1,11 @@
+%% Learn Your Way: Machine Learning for Navigation
+%
+% Author: Mateo Vanoy Marin & Silvia 
+% Date: 26/04/2018
+% Project: This code implements krigin interpolation to create an offline 
+% database for a fingerprint map (radio map. Using known RSS points from
+% the data.
+%
+% Log:
+% 26/04/2018 - Creation of script 
+%%
